132ddd99bd03ff7335bb25c68a12652dea020950
psingh <psingh@connecture.com>
2019-03-01 18:40:54 +0530

    c++ program on binary trees and bst

---
+++ DS/CheckBinaryTreeIfBst.cpp
@@ -0,0 +1,109 @@
+//CheckBinaryTreeIfBst.cpp
+#include <iostream>
+#include <math.h>
+#include <queue>
+//#include <stlib.h>
+using namespace std;
+
+struct BstNode{
+	int value;
+	struct BstNode* left;
+	struct BstNode* right;
+};
+
+void insert(int data);
+struct BstNode* GetNewNode(int data);
+void inorderTraversal();
+
+struct BstNode* root;
+
+int main()
+{
+	cout<<"Into the main method"<<"\n";
+	root = NULL;
+	// insert('M');
+	// insert('B');
+	// insert('Q');
+	// insert('Z');
+	// insert('A');
+	// insert('C');
+	insert(15);insert(10);insert(20);
+	insert(9);insert(11);insert(18);insert(27);
+	inorderTraversal();
+	//insert(27);
+	//levelOrderTraversal();
+}
+
+void inorderTraversal(struct BstNode* root)
+{
+	
+	queue<BstNode*> queueElements;
+	if(root == NULL) return;
+
+	inorderTraversal(root->left);
+	cout<<root->value<<", ";queueElements.push(root->value);
+	inorderTraversal(root->right);
+
+	int check = 1;
+	while(queueElements.front() != queueElements.back())
+	{
+		char temp = queueElements.front();
+		queueElements.pop();
+		
+		if(!(temp < queueElements.front()))
+		{
+			cout<<"the tree is not bst";check = 0;
+			break;
+		}
+	}
+	if(check)
+		cout<<"the tree is BST";
+}
+
+void insert(int data)
+{
+	if(root == NULL)
+	{
+		root = GetNewNode(data);
+		return;
+	}
+
+	struct BstNode* temp = root;
+	while(true)
+	{	
+		if(data <= temp->value)
+		{
+			if(temp->left == NULL)
+			{	
+				temp->left = GetNewNode(data);
+				return;	
+			}
+			else
+			{
+				temp = temp->left;
+			}
+		}
+		else if (data >= temp->value)
+		{
+			if(temp->right == NULL)
+			{	
+				temp->right = GetNewNode(data);
+				return;	
+			}
+			else
+			{
+				temp = temp->right;
+			}
+		}
+	}
+	free(temp);
+}
+
+struct BstNode* GetNewNode(int data)
+{
+	BstNode* newNode = new BstNode();
+	newNode->value = data;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	return newNode;
+}
